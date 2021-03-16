5beb6f85313a5fd56232bc84e132ea1e4d36b3c7
jivko25 <jivkou@yahoo.com>
2021-03-16 19:49:41 +0200

    fix

--- src/InheritanceTask1Person/Person.java
+++ src/InheritanceTask1Person/Person.java
@@ -1,8 +1,20 @@
 package InheritanceTask1Person;
 
-public abstract class Person {
+public class Person {
 	private String name;
-	private int age;
-	public abstract String getName();
-	public abstract int getAge();
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+	
 }
